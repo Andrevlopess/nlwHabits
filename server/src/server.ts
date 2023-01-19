b8d51026847037e2bbc48eb374ebfd969d0e33b0
André Lopes <andrellopes021@gmail.com>
2023-01-18 21:07:48 -0300

    aula2 backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,12 @@
 import Fastify, { fastify } from 'fastify'
-import {PrismaClient} from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './Routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-
-    const habits = await prisma.habit.findMany({
-        where: {
-            title:{
-                startsWith: 'Estudar'
-            }
-        }
-    })
-    return habits
-})
 
 app.listen({
     port: 3333
